bbee1609110565b80b5c08740e8efb76f236f71c
indrawijayakusuma <indrawk56@gmail.com>
2024-06-27 15:39:15 +0700

    fix otp validation payload

--- src/validator/otp/index.js
+++ src/validator/otp/index.js
@@ -3,7 +3,7 @@ const { OtpPayloadSchema } = require('./schema');
 const InvariantError = require('../../exceptions/InvariantError');
 
 const AuthenticationsValidator = {
-  validateOtpPayload: (payload) => {
+  validateOtpRegisterPayload: (payload) => {
     const validationResult = OtpPayloadSchema.validate(payload);
     if (validationResult.error) {
       throw new InvariantError(validationResult.error.message);
